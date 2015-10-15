a49716e5f2572e09436b1ef52d163ebcb9a3c9bb
david.martin <david.martin@editrack.com>
2015-10-15 11:01:08 +0100

    First push with c1 and  c2

---
+++ FirstConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+	class Program
+	{
+//first console
+		static void Main(string[] args)
+		{
+		}
+	}
+}
